dca94153881d658d43f27dc5e6a5525661d289ad
niexiaoguang <nxg@lohosoft.com>
2017-10-09 18:27:48 +0800

    passed wechat token test

--- src/pampa_api_handler.erl
+++ src/pampa_api_handler.erl
@@ -4,5 +4,21 @@
 -export([init/2]).
 
 init(Req, State) ->
-	io:print("~p",Req),
-	{ok, Req, State}.
+	% io:print("~p",Req),
+	Method = cowboy_req:method(Req),
+	handle(Method,Req,State).
+
+% for pass wechat token identification
+% handle(<<"GET">>,Req,State) ->
+% 	Qs1 = cowboy_req:parse_qs(Req),
+% 	{_,Echo} = lists:nth(2,Qs1),
+% 	Req1 = cowboy_req:reply(
+% 			200,
+% 			#{<<"content-type">> => <<"text/plain">>},
+% 			Echo,
+% 			Req
+% 			),
+% 	{ok,Req1,State}.
+
+handle(<<"GET">>,Req,State) ->
+	{ok,Req,State}.

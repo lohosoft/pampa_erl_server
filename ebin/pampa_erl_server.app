498b058b2fe998083812b31814b89c9ac0788a5e
niexiaoguang <nxg@lohosoft.com>
2017-10-09 20:31:07 +0800

    parse wechat qs done,encrypto token appid etc for get signature and decrypto post raw  xml data need more work

--- ebin/pampa_erl_server.app
+++ ebin/pampa_erl_server.app
@@ -1,7 +1,7 @@
 {application, 'pampa_erl_server', [
 	{description, "New project"},
 	{vsn, "0.1.0"},
-	{modules, ['pampa_api_handler','pampa_erl_server_app','pampa_erl_server_sup']},
+	{modules, ['pampa_api_handler','pampa_erl_server_app','pampa_erl_server_sup','wechat_utils']},
 	{registered, [pampa_erl_server_sup]},
 	{applications, [kernel,stdlib,cowboy,sync]},
 	{mod, {pampa_erl_server_app, []}},
--- src/pampa_api_handler.erl
+++ src/pampa_api_handler.erl
@@ -4,9 +4,34 @@
 -export([init/2]).
 
 init(Req, State) ->
-	% io:print("~p",Req),
-	Method = cowboy_req:method(Req),
-	handle(Method,Req,State).
+	% Qs = cowboy_req:parse_qs(Req),
+	% io:format("~p",[Qs]),
+	#{
+		<<"nonce">> := Nonce,
+		<<"timestamp">> := Timestamp,
+		% <<"openid">> := OpenID,
+		% <<"encrypt_type">> := EncryptType,
+		% <<"msg_signature">> := MsgSignature,
+		<<"signature">> := Signature
+
+	} = 
+		maps:from_list(cowboy_req:parse_qs(Req)),
+	io:format("~p \n",[Signature]),
+	io:format("~p \n",[Timestamp]),
+	io:format("~p \n",[Nonce]),
+	
+
+	Check = wechat_utils:check(binary_to_list(Signature), binary_to_list(Timestamp), binary_to_list(Nonce)),
+
+	io:format("check resutl is : ~p ",[Check]),
+	{ok,Req,State}.
+	% if
+	% 	Check == false ->
+	% 		{error,Req,State};
+	% 	true ->
+	% 		Method = cowboy_req:method(Req),
+	% 		handle(Method,Req,State)
+	% end.
 
 % for pass wechat token identification
 % handle(<<"GET">>,Req,State) ->
@@ -18,7 +43,25 @@ init(Req, State) ->
 % 			Echo,
 % 			Req
 % 			),
-% 	{ok,Req1,State}.
+	% {ok,Req1,State}.
 
 handle(<<"GET">>,Req,State) ->
-	{ok,Req,State}.
+	Req1 = cowboy_req:reply(
+		200,
+		#{<<"content-type">> => <<"text/plain">>},
+		<<"get echo">>,
+		Req
+		),
+	{ok,Req1,State};
+handle(<<"POST">>,Req,State) ->
+
+	io:format("~p : ~p \n",["POST Req",Req]),
+	Req1 = cowboy_req:reply(
+		200,
+		#{<<"content-type">> => <<"text/plain">>},
+		<<"post echo">>,
+		Req
+		),
+	{ok,Req1,State};
+handle(_,Req,State) ->
+	cowboy_req:reply(405,Req,State).
